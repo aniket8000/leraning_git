d8442efcae23894b0dedbda30df629a7a31fb62c
Aniket Kumar <aniketkumarvn@gmail.com>
2024-05-08 20:50:37 +0530

    some minor changes

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 print("this is my seond version ")
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+c=a+b
+if c>a:
+    print("c is greater")
+elif c>b:
+    print("c is greater")
+else:
+    print("c is not greater than a+b")
